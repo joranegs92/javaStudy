b870945a1c3b0e733434c5fdf5395633f41ebff6
joranegs92 <joranegs92@gmail.com>
2024-08-14 15:39:26 +0900

    3.Generic 특정 서브타입이 가능하게끔설정하는법 Comparable을 확장해서 compareTo ㄱㄱ

--- src/main/java/com/example/java/generic/Generics2.java
+++ src/main/java/com/example/java/generic/Generics2.java
@@ -1,13 +1,21 @@
 package com.example.java.generic;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class Generics2 <T extends List>{ // list서브타입만 가능하다
+public class Generics2 { // list서브타입만 가능하다
 
-	//static < Serializable & Comparator> void print(T t){} //타입 여러개도 가능하다.
+	/*
+	* comparable을 가진 타입만 사용할수 있게 지네릭을 사용하는 법
+	* 이렇게 정의내려주지 않으면 copareTo를 사용이 불가능하다
+	* */
+	static <T extends  Comparable<T>> long countGreatrrThan(T[] arr, T elem){
+		return Arrays.stream(arr).filter(s-> s.compareTo(elem)<0).count();
+	}
 	public static void main(String[] args) {
-
+		String[] arr = new String[] {"a","b","c","d"};
+		System.out.println(countGreatrrThan(arr,"c"));
 	}
 }
